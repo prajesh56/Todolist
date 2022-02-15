eeeb405582c991e0dd170fd2dce5d0fde656bc31
prajesh56 <pj.shakya10@gmail.com>
2022-02-15 18:38:30 +0545

    Create operation sucessful, use of params

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,21 @@ class ActivitiesController < ApplicationController
     #binding.pry
     @activities =Activity.all
   end
-  def create
-    
+  def new
+    @activities = Activity.new
   end
+
+  def create
+    @activities = Activity.new(activity_params)
+    if @activities.save
+      redirect_to @activities
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+    def activity_params
+      params.require(:Activity).permit(:task)
+    end
 end
