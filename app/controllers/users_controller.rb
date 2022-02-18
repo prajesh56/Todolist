f8597843a477d1020002387e490d09be06d1bed3
prajesh56 <pj.shakya10@gmail.com>
2022-02-18 10:21:04 +0545

    Create operation success in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @users = User.new
+  end
+
+  def create
+    @users = User.new(user_params)
+    if @users.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:address)
+    end
 end
