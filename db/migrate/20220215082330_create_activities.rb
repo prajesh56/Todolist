70bb8c4d5e1f2b0ac88bb2eefcb6696dd169d751
prajesh56 <pj.shakya10@gmail.com>
2022-02-18 12:55:34 +0545

    CRUD operation complete in users

--- db/migrate/20220215082330_create_activities.rb
+++ db/migrate/20220215082330_create_activities.rb
@@ -2,7 +2,7 @@ class CreateActivities < ActiveRecord::Migration[7.0]
   def change
     create_table :activities do |t|
       
-      t.string :task
+      t.string :task, null: false
       t.timestamps 
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_082330) do
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
-    t.string "task"
+    t.string "task", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
+    t.string "name", null: false
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
